037485cf914775ca8f40fc86dcedaf1f03f06df1
xiyyu <achmad.srim@gmail.com>
2020-11-12 09:08:03 +0700

    commit pertama

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import NavbarComponent from './components/NavbarComponent'
+import JumbotronComponent from './components/JumbotronComponent'
+import TableComponent from './components/TableComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    title: "Xiyyu Developer",
+    users: [
+      {
+        id: 1,
+        nama: "arya",
+        alamat: "pekalongan",
+        umur: 25,
+        nohp: "08596555366665",
+      },
+      {
+        id: 1,
+        nama: "arya",
+        alamat: "pekalongan",
+        umur: 25,
+        nohp: "08596555366665",
+      },
+      {
+        id: 1,
+        nama: "arya",
+        alamat: "pekalongan",
+        umur: 25,
+        nohp: "08596555366665",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div>
+        <NavbarComponent />
+        <JumbotronComponent title={this.state.title }/>
+        <TableComponent users={this.state.users}  />
+      </div>
+    )
+  }
 }
-
-export default App;
